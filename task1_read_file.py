a88e143db0c10adf60c239993b63de5acca95129
sakshibarate2604-collab <sakshibarate2604@gmail.com>
2026-02-13 19:38:47 +0530

    Implement file reading and error handling
    Add file reading functionality with error handling.

--- task1_read_file.py
+++ task1_read_file.py
@@ -1,1 +1,11 @@
+# Opening and reading the file
+    with open("sample.txt", "r") as file:
+        print("File content:\n")
+        
+ # Reading line by line
+        for line in file:
+            print(line.strip())
 
+# Handling error if file does not exist
+except FileNotFoundError:
+    print("Error: The file 'sample.txt' does not exist.")
